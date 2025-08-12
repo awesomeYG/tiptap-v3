cf49900c0385446c726b4cd33c02f220148c5f1b
yu.kuai <yu.kuai@chaitin.com>
2025-08-12 09:36:31 +0000

    @tiptap/html/server error

--- src/hook/fn.ts
+++ src/hook/fn.ts
@@ -1,6 +1,5 @@
 import { JSONContent } from "@tiptap/core"
 import { generateHTML, generateJSON } from "@tiptap/html"
-import { generateHTML as generateHTMLServer, generateJSON as generateJSONServer } from "@tiptap/html/server"
 import { renderToHTMLString, renderToMarkdown, renderToReactElement } from "@tiptap/static-renderer"
 import { getExtensions } from "../extension"
 
@@ -20,8 +19,6 @@ export const useFn = ({
   return {
     getJSONByHTML: (html: string) => generateJSON(html, extensions),
     getHTMLByJSON: (json: JSONContent) => generateHTML(json, extensions),
-    getHTMLServerByJSON: (json: JSONContent) => generateHTMLServer(json, extensions),
-    getJSONServerByHTML: (html: string) => generateJSONServer(html, extensions),
     getStaticRenderToHTMLStringByJSON: (json: JSONContent) => renderToHTMLString({ content: json, extensions }),
     getStaticRenderToMarkdownByJSON: (json: JSONContent) => renderToMarkdown({ content: json, extensions }),
     getStaticRenderToReactElementByJSON: (json: JSONContent) => renderToReactElement({ content: json, extensions }),
