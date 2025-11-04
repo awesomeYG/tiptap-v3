93dded0b2bab67026f8144749a42398f8e1edeeb
yu.kuai <yu.kuai@chaitin.com>
2025-11-04 11:43:34 +0800

    fix: ts export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './component/Icons';
 export { default as Editor } from './Editor';
 export { default as EditorDiff } from './EditorDiff';
-export { default as EditorMarkdown, MarkdownEditorRef } from './EditorMarkdown';
+export { default as EditorMarkdown, type MarkdownEditorRef } from './EditorMarkdown';
 export { default as EditorThemeProvider } from './EditorThemeProvider';
 export { default as EditorToolbar } from './EditorToolbar';
 export { default as useTiptap } from './hook';
