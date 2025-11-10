c6908cf677a4d78b41bac1b0dae0e588fee421d2
yu.kuai <yu.kuai@chaitin.com>
2025-11-10 19:11:48 +0800

    fix: alert

--- src/extension/component/Alert/index.tsx
+++ src/extension/component/Alert/index.tsx
@@ -1,10 +1,9 @@
-import { ActionDropdown, HoverPopover } from '@ctzhian/tiptap/component'
+import { FloatingPopover } from '@ctzhian/tiptap/component'
 import { CheckboxCircleFillIcon, CloseCircleFillIcon, ErrorWarningFillIcon, Information2FillIcon, ScrollToBottomLineIcon, TextIcon, UserSmileFillIcon } from '@ctzhian/tiptap/component/Icons'
 import { ToolbarItem } from '@ctzhian/tiptap/component/Toolbar'
-import { MenuItem } from '@ctzhian/tiptap/type'
 import { Box, Divider, Stack } from '@mui/material'
 import { NodeViewContent, NodeViewProps, NodeViewWrapper } from '@tiptap/react'
-import React, { useMemo, useRef } from 'react'
+import React, { useMemo, useRef, useState } from 'react'
 
 export interface AlertAttributes {
   id?: string
@@ -38,6 +37,7 @@ const VARIANT_DATA: Record<string, { icon: React.ReactNode, color: string }> = {
 const AlertView: React.FC<NodeViewProps> = ({ editor, node, updateAttributes, selected }) => {
   const attrs = node.attrs as AlertAttributes
   const containerRef = useRef<HTMLDivElement | null>(null)
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
 
   const showIcon = attrs.type !== 'text'
 
@@ -45,110 +45,107 @@ const AlertView: React.FC<NodeViewProps> = ({ editor, node, updateAttributes, se
     return VARIANT_DATA[attrs.variant || 'info']
   }, [attrs.variant])
 
-  const operationMenu = (
-    <Stack direction={'row'} alignItems={'center'} sx={{ p: 0.5 }}>
-      <ToolbarItem
-        icon={<Information2FillIcon sx={{ fontSize: '1rem', color: 'primary.main' }} />}
-        tip='信息'
-        onClick={() => updateAttributes({ variant: 'info' })}
-        className={attrs.variant === 'info' ? 'tool-active' : ''}
-      />
-      <ToolbarItem
-        icon={<ErrorWarningFillIcon sx={{ fontSize: '1rem', color: 'warning.main' }} />}
-        tip='警告'
-        onClick={() => updateAttributes({ variant: 'warning' })}
-        className={attrs.variant === 'warning' ? 'tool-active' : ''}
-      />
-      <ToolbarItem
-        icon={<CloseCircleFillIcon sx={{ fontSize: '1rem', color: 'error.main' }} />}
-        tip='错误'
-        onClick={() => updateAttributes({ variant: 'error' })}
-        className={attrs.variant === 'error' ? 'tool-active' : ''}
-      />
-      <ToolbarItem
-        icon={<CheckboxCircleFillIcon sx={{ fontSize: '1rem', color: 'success.main' }} />}
-        tip='成功'
-        onClick={() => updateAttributes({ variant: 'success' })}
-        className={attrs.variant === 'success' ? 'tool-active' : ''}
-      />
-      <ToolbarItem
-        icon={<UserSmileFillIcon sx={{ fontSize: '1rem', color: 'text.disabled' }} />}
-        tip='默认'
-        onClick={() => updateAttributes({ variant: 'default' })}
-        className={attrs.variant === 'default' ? 'tool-active' : ''}
-      />
-
-      <Divider orientation='vertical' flexItem sx={{ height: '1rem', mx: 0.5, alignSelf: 'center', borderColor: 'divider' }} />
-
-      <ActionDropdown
-        list={[
-          {
-            key: 'text',
-            label: '文字',
-            icon: <TextIcon sx={{ fontSize: '1rem' }} />,
-            onClick: () => updateAttributes({ type: 'text' }),
-          },
-          {
-            key: 'icon',
-            label: '图标文字',
-            icon: <ScrollToBottomLineIcon sx={{ transform: 'rotate(90deg)', fontSize: '1rem' }} />,
-            onClick: () => updateAttributes({ type: 'icon' }),
-          },
-        ] as MenuItem[]}
-        selected={attrs.type || 'icon'}
-        tip='类型'
-      />
-    </Stack>
-  )
+  const handleShowOperationPopover = (event: React.MouseEvent<HTMLDivElement>) => setAnchorEl(event.currentTarget)
+  const handleCloseOperationPopover = () => setAnchorEl(null)
 
   return (
-    <HoverPopover
-      actions={operationMenu}
-      hoverDelay={500}
-      closeDelay={300}
-      placement="top"
-      disabled={!editor.isEditable}
+    <NodeViewWrapper
+      ref={containerRef}
+      className={`alert-wrapper ${selected ? 'ProseMirror-selectednode' : ''}`}
+      data-drag-handle
+      as={'div'}
+      style={{
+        marginLeft: (node.attrs as any)?.indent ? (node.attrs as any).indent * 32 : undefined,
+        border: '1px solid',
+        borderColor: variantData.color,
+        // color: attrs.variant === 'default' ? 'var(--mui-palette-text-primary)' : variantData.color,
+        borderRadius: '10px',
+        padding: '12px 16px',
+        lineHeight: 1.625,
+        display: 'flex',
+        alignItems: 'flex-start',
+        gap: '16px',
+        background: attrs.variant === 'default' ? 'var(--mui-palette-background-paper3)' : `color-mix(in srgb, ${variantData.color} 10%, transparent)`,
+      }}
+      onClick={handleShowOperationPopover}
     >
-      <NodeViewWrapper
-        ref={containerRef}
-        className={`alert-wrapper ${selected ? 'ProseMirror-selectednode' : ''}`}
-        data-drag-handle
-        as={'div'}
-        style={{
-          marginLeft: (node.attrs as any)?.indent ? (node.attrs as any).indent * 32 : undefined,
-          border: '1px solid',
-          borderColor: `color-mix(in srgb, ${variantData.color} 50%, transparent)`,
-          borderRadius: '10px',
-          padding: '16px',
-          lineHeight: 1.625,
-          display: 'flex',
-          alignItems: 'flex-start',
-          gap: '16px',
-          background: attrs.variant === 'default' ? 'var(--mui-palette-background-paper3)' : `color-mix(in srgb, ${variantData.color} 10%, transparent)`,
-        }}
-      >
-        {showIcon && (
-          <Box sx={{
-            pt: '2px',
-            lineHeight: 1,
-            color: attrs.variant === 'default' ? 'text.disabled' : variantData.color,
-          }}>{variantData.icon}</Box>
-        )}
+      {showIcon && (
         <Box sx={{
-          flex: 1,
-          width: 0,
-          'code': {
-            borderColor: attrs.variant === 'default' ? '' : `color-mix(in srgb, ${variantData.color} 30%, transparent) !important`,
-            bgcolor: attrs.variant === 'default' ? '' : `color-mix(in srgb, ${variantData.color} 10%, transparent) !important`,
-          }
-        }}>
-          <NodeViewContent as={'div'} />
-        </Box>
-      </NodeViewWrapper>
-    </HoverPopover>
+          pt: '2px',
+          lineHeight: 1,
+          color: attrs.variant === 'default' ? 'text.disabled' : variantData.color,
+        }}>{variantData.icon}</Box>
+      )}
+      <Box sx={{
+        flex: 1,
+        width: 0,
+        'code': {
+          borderColor: attrs.variant === 'default' ? '' : `color-mix(in srgb, ${variantData.color} 30%, transparent) !important`,
+          bgcolor: attrs.variant === 'default' ? '' : `color-mix(in srgb, ${variantData.color} 10%, transparent) !important`,
+        }
+      }}>
+        <NodeViewContent as={'div'} />
+      </Box>
+
+      {editor.isEditable && (
+        <FloatingPopover
+          open={Boolean(anchorEl)}
+          anchorEl={anchorEl}
+          onClose={handleCloseOperationPopover}
+          placement="top"
+        >
+          <Stack direction={'row'} alignItems={'center'} sx={{ p: 0.5 }}>
+            <ToolbarItem
+              icon={<Information2FillIcon sx={{ fontSize: '1rem', color: 'primary.main' }} />}
+              tip='信息'
+              onClick={() => updateAttributes({ variant: 'info' })}
+              className={attrs.variant === 'info' ? 'tool-active' : ''}
+            />
+            <ToolbarItem
+              icon={<ErrorWarningFillIcon sx={{ fontSize: '1rem', color: 'warning.main' }} />}
+              tip='警告'
+              onClick={() => updateAttributes({ variant: 'warning' })}
+              className={attrs.variant === 'warning' ? 'tool-active' : ''}
+            />
+            <ToolbarItem
+              icon={<CloseCircleFillIcon sx={{ fontSize: '1rem', color: 'error.main' }} />}
+              tip='错误'
+              onClick={() => updateAttributes({ variant: 'error' })}
+              className={attrs.variant === 'error' ? 'tool-active' : ''}
+            />
+            <ToolbarItem
+              icon={<CheckboxCircleFillIcon sx={{ fontSize: '1rem', color: 'success.main' }} />}
+              tip='成功'
+              onClick={() => updateAttributes({ variant: 'success' })}
+              className={attrs.variant === 'success' ? 'tool-active' : ''}
+            />
+            <ToolbarItem
+              icon={<UserSmileFillIcon sx={{ fontSize: '1rem', color: 'text.disabled' }} />}
+              tip='默认'
+              onClick={() => updateAttributes({ variant: 'default' })}
+              className={attrs.variant === 'default' ? 'tool-active' : ''}
+            />
+
+            <Divider orientation='vertical' flexItem sx={{ height: '1rem', mx: 0.5, alignSelf: 'center', borderColor: 'divider' }} />
+
+            <ToolbarItem
+              icon={<TextIcon sx={{ fontSize: '1rem' }} />}
+              tip='纯文字'
+              onClick={() => updateAttributes({ type: 'text' })}
+              className={attrs.type === 'text' ? 'tool-active' : ''}
+            />
+            <ToolbarItem
+              icon={<ScrollToBottomLineIcon sx={{ transform: 'rotate(90deg)', fontSize: '1rem' }} />}
+              tip='图标文字'
+              onClick={() => updateAttributes({ type: 'icon' })}
+              className={attrs.type === 'icon' ? 'tool-active' : ''}
+            />
+          </Stack>
+        </FloatingPopover>
+      )}
+    </NodeViewWrapper>
   )
 }
 
 export default AlertView
-
 
