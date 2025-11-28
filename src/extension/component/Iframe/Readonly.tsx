c5221c629903b49abab1e0217158d3cfd6ef481c
yu.kuai <yu.kuai@chaitin.com>
2025-11-28 18:24:49 +0800

    fix: iframe Readonly 组件需要用 NodeViewWrapper 包裹

--- src/extension/component/Iframe/Readonly.tsx
+++ src/extension/component/Iframe/Readonly.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@mui/material'
+import { NodeViewWrapper } from '@tiptap/react'
 import React from 'react'
 
 export type IframeAttributes = {
@@ -10,23 +11,25 @@ export type IframeAttributes = {
 
 const ReadonlyIframe = ({ attrs }: { attrs: IframeAttributes }) => {
   return (
-    <Box sx={{
-      textAlign: attrs.align || undefined,
-    }}>
-      <iframe
-        src={attrs.src}
-        width={typeof attrs.width === 'number' ? attrs.width : undefined}
-        height={attrs.height}
-        style={{
-          display: 'inline-block',
-          border: 0,
-          width: typeof attrs.width === 'string' && attrs.width.endsWith('%') ? attrs.width : undefined,
-          maxWidth: '100%'
-        }}
-        frameBorder="0"
-        allowFullScreen
-      />
-    </Box>
+    <NodeViewWrapper className={`iframe-wrapper`}>
+      <Box sx={{
+        textAlign: attrs.align || undefined,
+      }}>
+        <iframe
+          src={attrs.src}
+          width={typeof attrs.width === 'number' ? attrs.width : undefined}
+          height={attrs.height}
+          style={{
+            display: 'inline-block',
+            border: 0,
+            width: typeof attrs.width === 'string' && attrs.width.endsWith('%') ? attrs.width : undefined,
+            maxWidth: '100%'
+          }}
+          frameBorder="0"
+          allowFullScreen
+        />
+      </Box>
+    </NodeViewWrapper>
   )
 }
 
